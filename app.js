093d232cd654c4d9dfaffb607d88eed363dcae1d
lamine785260388 <lamine785260388@gmail.com>
2022-07-02 12:43:07 +0000

    2e

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-app.listen(port,()=>{
+const http =require("http");
+function response(req, res) { 
+    res.status(200).send({nom:"uadb"});
+    
+ };
+app.get("/get",response);
+app.get("/",response);
+app.get("/essai",(req,res)=>{
+    res.send("bonjour");
+    
+})
+ http.createServer(app).listen(port,function name(){
     console.log("Application start at : localhost"+port);
-});
+ })
+// app.listen(port, ()=>{
+//     console.log("Application start at : localhost"+port);
+// });
