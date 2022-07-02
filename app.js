ad98d8f2de5e36234c99f341d2b6bd6c6d0caec9
lamine785260388 <lamine785260388@gmail.com>
2022-07-02 16:44:52 +0000

    3e

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+
 const http =require("http");
+require("dotenv").config();
+const port = process.env.PORT ;
+
 function response(req, res) { 
+  
     res.status(200).send({nom:"uadb"});
     
+    
  };
+ const bodyParser=require("body-parser")
+ app.use(bodyParser.json());
+ 
 app.get("/get",response);
 app.get("/",response);
 app.get("/essai",(req,res)=>{
     res.send("bonjour");
     
 })
+function enregistrer(req,res){
+
+    console.log(req.body);
+    res.send(req.body);
+
+}
+app.post("/enregistrer/:name/:id",enregistrer)
+// app.get("/enregistrer",enregistrer)
  http.createServer(app).listen(port,function name(){
     console.log("Application start at : localhost"+port);
+    
  })
 // app.listen(port, ()=>{
 //     console.log("Application start at : localhost"+port);
